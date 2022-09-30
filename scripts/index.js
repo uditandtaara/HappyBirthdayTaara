a93a7b48940f6b324778fab6f21bfc909077fd51
uditandtaara <114770128+uditandtaara@users.noreply.github.com>
2022-10-01 00:16:23 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -48,7 +48,7 @@ x = setInterval(function () {
     hw = w / 2, // half-width
     hh = h / 2,
     opts = {
-      strings: ['HAPPY', 'BIRTHDAY', config.name],
+      strings: ['HAPPY', 'BIRTHDAY   ', config.name],
       charSize: 30,
       charSpacing: 35,
       lineHeight: 40,
