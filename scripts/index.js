c1140bde55e1bec5cbc605f420e5763865f690d3
uditandtaara <114770128+uditandtaara@users.noreply.github.com>
2022-10-01 19:10:26 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date(`${config.birthdate} 18:59:00`).getTime();
+let countDown = new Date(`${config.birthdate} 20:59:00`).getTime();
 x = setInterval(function () {
   let now = new Date().getTime(),
     distance = countDown - now;
